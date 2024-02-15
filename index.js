256ae6edb3baf26dccb2b8fd2a96bde2eea6005e
Eeva <eeva.hanka@iki.fi>
2024-02-15 18:18:37 +0200

    3.1-3.6

--- index.js
+++ index.js
@@ -4,75 +4,123 @@ const app = express()
 
 app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+let persons = [
+{ 
+  "id": 1,
+  "name": "Arto Hellas", 
+  "number": "040-123456"
+},
+{ 
+  "id": 2,
+  "name": "Ada Lovelace", 
+  "number": "39-44-5323523"
+},
+{ 
+  "id": 3,
+  "name": "Dan Abramov", 
+  "number": "12-43-234345"
+},
+{ 
+  "id": 4,
+  "name": "Mary Poppendieck", 
+  "number": "39-23-6423122"
+}
 ]
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes/:id', (request, response) => {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   console.log(id)
-  const note = notes.find(note => note.id === id)
-  console.log(note)
-  if (note) {
-    response.json(note)
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+  if (person) {
+    response.json(person)
   } else {
     response.status(404).end()
   }
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
+
+  const id = getRandomInt(999999)
+  return id
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if ((!body.name) || (!body.number)){
     return response.status(400).json({ 
       error: 'content missing' 
     })
   }
+  for ( peep of persons) {
+    if (body.name == peep.name) {
+      return response.status(400).json({ 
+        error: 'name allready in phonebook' 
+    })
+  }
+}
 
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
+  const person = {
     id: generateId(),
+    name: body.name,
+    number: body.number
   }
 
-  notes = notes.concat(note)
+  persons = persons.concat(person)
 
-  response.json(note)
+  response.json(person)
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const get_date_str = () =>{
+  const date = new Date()
+  console.log(date)
+  return(date)
+}
+
+const getInfoHtml = (nro_of_persons, date) => {
+  console.log(date)
+  return(`
+<!DOCTYPE html>
+    <html>
+      <head>
+      </head>
+      <body>
+        <div>
+          <p>phonebook has info for ${nro_of_persons} people</p>
+          <p> ${date}</p>
+
+        </div>
+      </body>
+    </html>
+`)
+} 
+
+app.get('/info', (request, response)=> {
+  const nro_of_persons = persons.length
+  const date = get_date_str()
+  console.log(date)
+  response.send(
+    getInfoHtml(nro_of_persons, date)
+  )
 })
 
 const PORT = 3001
